04e7bc27debfb6a47d681f1a6cf2ba91a4ea1a41
Mark <mark.reyta@dailypress.com.au>
2022-05-11 14:21:11 +0800

    - Fixed migration script

--- database/migrations/Version20220510091832.php
+++ database/migrations/Version20220510091832.php
@@ -14,13 +14,8 @@ class Version20220510091832 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE file_uploads CHANGE format  format VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE package_procedures ADD CONSTRAINT FK_9A860BB71624BCD2 FOREIGN KEY (procedure_id) REFERENCES procedures (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_9A860BB71624BCD2 ON package_procedures (procedure_id)');
         $this->addSql('ALTER TABLE patient_visits CHANGE patient_bp patient_bp VARCHAR(255) DEFAULT NULL, CHANGE patient_height patient_height VARCHAR(255) DEFAULT NULL, CHANGE patient_weight patient_weight VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE procedures DROP FOREIGN KEY FK_969AFE421624BCD2');
-        $this->addSql('DROP INDEX UNIQ_969AFE421624BCD2 ON procedures');
-        $this->addSql('ALTER TABLE procedures DROP procedure_id');
+
     }
 
     /**
@@ -29,13 +24,6 @@ class Version20220510091832 extends AbstractMigration
     public function down(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE file_uploads CHANGE  format format VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE package_procedures DROP FOREIGN KEY FK_9A860BB71624BCD2');
-        $this->addSql('DROP INDEX UNIQ_9A860BB71624BCD2 ON package_procedures');
         $this->addSql('ALTER TABLE patient_visits CHANGE patient_bp patient_bp VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE patient_height patient_height VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE patient_weight patient_weight VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE procedures ADD procedure_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE procedures ADD CONSTRAINT FK_969AFE421624BCD2 FOREIGN KEY (procedure_id) REFERENCES package_procedures (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_969AFE421624BCD2 ON procedures (procedure_id)');
     }
 }
